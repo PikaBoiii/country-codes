3b8b96ea05ed1e35411ff7dc407ef3cd9dbdba81
PikaBoiii <pauldroigk@gmail.com>
2022-05-06 14:08:14 +0200

    added country codes

---
+++ index.js
@@ -0,0 +1,27 @@
+module.exports.searchCode = function (countryCode, language) {
+    let lang;
+    let country;
+
+    //check if language is available
+    try {
+        lang = require('./languages/' + language + '.json');
+    } catch {
+        lang = require('./languages/en.json');
+    }
+
+    //search for country code
+    country = lang.find(country => country.country_code === countryCode.toUpperCase());
+    if (country) {
+        return country;
+    }
+
+    //fallback if country is not found in that language
+    lang = require('./languages/en.json');
+    country = lang.find(country => country.country_code === countryCode.toUpperCase());
+    if (country) {
+        return country;
+    } else {
+        return null;
+    }
+
+}
